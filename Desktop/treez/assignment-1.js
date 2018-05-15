5fe87d17f73a9df3301f84c6d822e5d4854c9bce
nathanlovegrove <nathanlovegrove@hotmail.com>
2018-05-14 23:19:54 -0400

    assignment-1.js

---
+++ Desktop/treez/assignment-1.js
@@ -0,0 +1,26 @@
+var numbers = ['1','2','3','4','5','6','7','8','9']
+
+
+numbers.push['1']
+numbers.push['2']
+numbers.push['3']
+numbers.push['4']
+numbers.push['5']
+numbers.push['6']
+numbers.push['7']
+numbers.push['8']
+numbers.push['9']
+
+for (var i=0; i < 10; i++){
+    console.log(i);
+}
+console.log('loop is done');
+
+console.log(numbers[0])
+console.log(numbers[8])
+
+var car = {
+    prop: "colour"
+}
+
+car.colour = ("blank")
